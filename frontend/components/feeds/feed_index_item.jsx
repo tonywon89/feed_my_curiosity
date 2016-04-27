6ef505d4ac1d418e634e68fd3f4a69c92fdad8e5
Tony Won <tonywon89@gmail.com>
2016-04-27 11:38:14 -0700

    Add more styling to 'Add Feed' button in FeedIndexItem

--- frontend/components/feeds/feed_index_item.jsx
+++ frontend/components/feeds/feed_index_item.jsx
@@ -5,8 +5,9 @@ var FeedIndexItem = React.createClass({
     var feed = this.props.feed;
     return (
       <div className="feed-index-item">
+
         <a href={feed.url} className="feed-index-item-title">{feed.title}</a>
-        <button className="feed-index-add-btn">Add</button>
+        <button className="feed-index-add-btn">Add Feed</button>
         <p className="feed-index-item-description">{feed.description}</p>
       </div>
     );
