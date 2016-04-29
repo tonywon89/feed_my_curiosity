8513e6e447b8a81728766769099af14a6caf8a67
Tony Won <tonywon89@gmail.com>
2016-04-28 18:17:35 -0700

    Add functionality to angle button being pressed in sidebar

--- frontend/components/sidebars/sidebar_collection_index_item.jsx
+++ frontend/components/sidebars/sidebar_collection_index_item.jsx
@@ -1,13 +1,30 @@
 var React = require("react");
 
 var SidebarCollectionIndexItem = React.createClass({
+  getInitialState: function () {
+    return { isOpen: false };
+  },
+
+  toggleOpen: function () {
+    if (this.state.isOpen) {
+      this.setState({ isOpen : false });
+    } else {
+      this.setState({ isOpen : true });
+    }
+  },
+
   render: function () {
 
-    var collectionItemIconClass = "fa fa-angle-right collection-index-item-icon";
+    var collectionItemIconClass;
+    if (this.state.isOpen) {
+      collectionItemIconClass = "fa fa-angle-down collection-index-item-icon";
+    } else {
+      collectionItemIconClass = "fa fa-angle-right collection-index-item-icon";
+    }
 
     return (
       <div className="collection-index-item">
-        <i className={collectionItemIconClass}></i>
+        <i className={collectionItemIconClass} onClick={this.toggleOpen}></i>
         {this.props.collection.name}
       </div>
     );
