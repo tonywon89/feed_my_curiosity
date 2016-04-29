025f2ea2e0dd3fcbd3ff2d48a856bcb4f956aab1
Tony Won <tonywon89@gmail.com>
2016-04-29 16:06:39 -0700

    Add pencil and times icons for collection title

--- frontend/components/collections/collection_index_item.jsx
+++ frontend/components/collections/collection_index_item.jsx
@@ -4,7 +4,13 @@ var CollectionIndexItem = React.createClass({
   render: function () {
     return (
       <div className="collection-index-item">
-        <h1 className="collection-item-title">{this.props.collection.name}</h1>
+        <h1 className="collection-item-title">
+          <h2>{this.props.collection.name}</h2>
+          <div className="edit-icons">
+            <i className="fa fa-pencil pencil"></i>
+            <i className="fa fa-times times"></i>
+          </div>
+        </h1>
       </div>
     );
   }
