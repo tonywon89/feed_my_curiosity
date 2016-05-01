e536ca1d31ad48c186297fe7f2f8e6c83a5a23c3
Tony Won <tonywon89@gmail.com>
2016-04-30 19:26:30 -0700

    Display content of articles in article index item

--- frontend/components/articles/article_index_item.jsx
+++ frontend/components/articles/article_index_item.jsx
@@ -8,16 +8,45 @@ var ArticleIndexItem = React.createClass({
     var image;
     image = imageUrl ? <img src={imageUrl} /> : <img src="http://dummyimage.com/600x400/46b0a4/414582.png&text=Curiously,+there+is+no+image" />;
 
+    var content = getContent(entry);
+
     return (
       <div className="article-index-item">
         <div>
           {image}
         </div>
         <h4>{entry.title}</h4>
+        <p>{content}</p>
       </div>
     );
   }
 });
+
+
+var getContent = function (entry) {
+  var dummyEl = document.createElement('div');
+  var content;
+
+  if (entry.summary) {
+    dummyEl.innerHTML = entry.summary;
+    var paragraphs = dummyEl.getElementsByTagName('p');
+    if (paragraphs.length !== 0) {
+      content = paragraphs[0].innerText.length >= paragraphs[1].innerText.length ? paragraphs[0].innerText : paragraphs[1].innerText;
+    } else {
+      if (entry.content) {
+        dummyEl.innerHTML = entry.content;
+        paragraphs = dummyEl.getElementsByTagName('p');
+
+        if (paragraphs.length !== 0) {
+
+          content = paragraphs[0].innerText.length >= paragraphs[1].innerText.length ? paragraphs[0].innerText : paragraphs[1].innerText;
+        }
+      }
+    }
+  }
+
+  return content;
+};
 
 var getImageUrl = function (entry) {
   var imgRegex = /src="([^"]+)"/;
