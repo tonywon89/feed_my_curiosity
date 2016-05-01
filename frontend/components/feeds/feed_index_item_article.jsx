f43c255201275baf4bd7795639e9dcad97efe918
Tony Won <tonywon89@gmail.com>
2016-04-30 23:40:19 -0700

    Add previous fix to feed_index-item_article

--- frontend/components/feeds/feed_index_item_article.jsx
+++ frontend/components/feeds/feed_index_item_article.jsx
@@ -37,8 +37,15 @@ var getImageUrl = function (entry) {
         }
       }
     }
-  }
+  } else {
+    if (entry.content) {
+      imageUrl = entry.content.match(imgRegex);
 
+      if (imageUrl) {
+        imageUrl = imageUrl[1];
+      }
+    }
+  }
   return imageUrl;
 };
 
