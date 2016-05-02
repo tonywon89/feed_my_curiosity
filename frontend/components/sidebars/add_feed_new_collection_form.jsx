7091a6cbaab9ec79728fba901bd7208f6cc8d619
Tony Won <tonywon89@gmail.com>
2016-05-02 08:12:33 -0700

    Implement create collection from within new feed sidebar

--- frontend/components/sidebars/add_feed_new_collection_form.jsx
+++ frontend/components/sidebars/add_feed_new_collection_form.jsx
@@ -1,11 +1,31 @@
 var React = require("react");
 
+var CollectionClientActions = require("../../actions/collection/collection_client_actions");
+
 var AddFeedNewCollectionForm = React.createClass({
+  getInitialState: function () {
+    return { name: "" };
+  },
+
+  nameChange: function (event) {
+    this.setState({ name: event.target.value });
+  },
+
+  handleSubmit: function (event) {
+    event.preventDefault();
+    var collection = {name: this.state.name };
+    CollectionClientActions.createCollection(collection);
+    this.setState({ name: "" });
+  },
+
   render: function () {
     return (
       <li className="add-feed-collection new-collection-form">
-        <form>
-          <input type="text" placeholder="Name of collection" />
+        <form onSubmit={this.handleSubmit}>
+          <input type="text"
+                 placeholder="Name of collection"
+                 value={this.state.name}
+                 onChange={this.nameChange}/>
           <input type="submit" value="Create" />
         </form>
       </li>
