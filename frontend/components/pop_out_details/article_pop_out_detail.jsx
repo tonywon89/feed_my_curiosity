b1290edece28a6829efefb3540e2f0791c509a1f
Tony Won <tonywon89@gmail.com>
2016-05-03 10:28:27 -0700

    Add and style title and author for article pop out detail

--- frontend/components/pop_out_details/article_pop_out_detail.jsx
+++ frontend/components/pop_out_details/article_pop_out_detail.jsx
@@ -2,8 +2,13 @@ var React = require("react");
 
 var ArticlePopOutDetail = React.createClass({
   render: function () {
+    var entry = this.props.entry;
+
     return (
-      <div>{this.props.entry.title}</div>
+      <div className="article-pop-out-detail">
+        <h1 className="article-pop-out-header">{entry.title}</h1>
+        <h6>By {entry.author}</h6>
+      </div>
     );
   }
 });
