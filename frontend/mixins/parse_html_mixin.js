5d0cc77c0d8dd98113f0cc154ba6ef174951066f
Tony Won <tonywon89@gmail.com>
2016-05-05 17:26:03 -0700

    Refactor the parse html mixin

--- frontend/mixins/parse_html_mixin.js
+++ frontend/mixins/parse_html_mixin.js
@@ -1,10 +1,9 @@
-
 var ParseHTMLMixin = {
   getContent: function (entry) {
     var content;
-    content = this.getDiv(entry);
+    content = getDiv(entry);
     if (!content) {
-      content = this.getParagraph(entry);
+      content = getParagraph(entry);
     }
     return content;
   },
@@ -13,101 +12,110 @@ var ParseHTMLMixin = {
     var imgRegex = /src="([^"]+)"/;
     var imageUrl;
 
-    if (entry.summary) {
-      imageUrl = entry.summary.match(imgRegex);
+    imageUrl = checkSummary(entry, imgRegex);
 
-      if (imageUrl) {
-
-        if (imageUrl[1].indexOf("http://feeds.feedburner.com/") === -1 && imageUrl[1].indexOf("youtube") === -1) {
-          imageUrl = imageUrl[1];
-        } else {
-          imageUrl = undefined;
-        }
-      } else {
-        if (entry.content) {
-          imageUrl = entry.content.match(imgRegex);
-
-          if (imageUrl) {
-
-            if (imageUrl[1].indexOf("http://feeds.feedburner.com/") === -1 && imageUrl[1].indexOf("youtube") === -1) {
-              imageUrl = imageUrl[1];
-            } else {
-              imageUrl = undefined;
-            }
-          }
-        }
-      }
-    } else {
-      if (entry.content) {
-        imageUrl = entry.content.match(imgRegex);
-
-        if (imageUrl) {
-          if (imageUrl[1].indexOf("http://feeds.feedburner.com/") === -1 && imageUrl[1].indexOf("youtube") === -1) {
-            imageUrl = imageUrl[1];
-          } else {
-            imageUrl = undefined;
-          }
-        }
-      }
+    if (!imageUrl) {
+      imageUrl = checkContent(entry, imgRegex);
     }
+
     return imageUrl;
-  },
-
-  getParagraph: function (entry) {
-    var paragraph;
-    if (entry.summary) {
-      paragraph = this.getInnerText(entry.summary, 'p');
-      if (!paragraph) {
-        if (entry.content) {
-          paragraph = this.getInnerText(entry.content, 'p');
-        }
-      }
-    } else {
-      if (entry.content) {
-        paragraph = this.getInnerText(entry.content, 'p');
-      }
-    }
-    return paragraph;
-  },
-
-  getDiv: function (entry) {
-    var div;
-    if (entry.summary) {
-      div = this.getInnerText(entry.summary, 'div');
-
-      if (!div) {
-        if (entry.content) {
-          div = this.getInnerText(entry.content, 'div');
-        }
-      }
-    } else {
-      if (entry.content) {
-        div = this.getInnerText(entry.content, 'div');
-      }
-    }
-
-    return div;
-  },
-
-  getInnerText:  function (htmlString, selector) {
-    var dummyEl = document.createElement('div');
-    dummyEl.innerHTML = htmlString;
-    if (selector === 'div') {
-      return dummyEl.innerText;
-    } else {
-      var result = dummyEl.getElementsByTagName(selector);
-    }
-
-    var innerText;
-    if (result.length > 1) {
-      innerText = result[0].innerText.length >= result[1].innerText.length ? result[0].innerText : result[1].innerText;
-    } else if (result.length > 0) {
-      innerText = result[0].innerText;
-    }
-    debugger;
-
-    return innerText;
   }
+};
+
+var checkSummary = function (entry, regex) {
+  var imageUrl;
+
+  if (entry.summary) {
+    imageUrl = checkItemForUrl(entry.summary, regex);
+
+    if (!imageUrl) {
+      imageUrl = checkContent(entry, regex);
+    }
+  }
+
+  return imageUrl;
+};
+
+var checkContent = function (entry, regex) {
+  var imageUrl;
+  if (entry.content) {
+    imageUrl = checkItemForUrl(entry.content, regex);
+  }
+  return imageUrl;
+};
+
+var checkItemForUrl = function (entry, regex) {
+  var imageUrl = entry.match(regex);
+  if (imageUrl) {
+    imageUrl = setImageUrl(imageUrl);
+  }
+  return imageUrl;
+};
+
+var setImageUrl = function (imageUrl) {
+  var result;
+  if (imageUrl[1].indexOf("http://feeds.feedburner.com/") === -1 && imageUrl[1].indexOf("youtube") === -1) {
+    result = imageUrl[1];
+  } else {
+    result = undefined;
+  }
+  return result;
+};
+
+var getParagraph = function (entry) {
+  var paragraph;
+  if (entry.summary) {
+    paragraph = getInnerText(entry.summary, 'p');
+    if (!paragraph) {
+      if (entry.content) {
+        paragraph = getInnerText(entry.content, 'p');
+      }
+    }
+  } else {
+    if (entry.content) {
+      paragraph =
+      getInnerText(entry.content, 'p');
+    }
+  }
+  return paragraph;
+};
+
+var getDiv = function (entry) {
+  var div;
+  if (entry.summary) {
+    div = getInnerText(entry.summary, 'div');
+
+    if (!div) {
+      if (entry.content) {
+        div = getInnerText(entry.content, 'div');
+      }
+    }
+  } else {
+    if (entry.content) {
+      div = getInnerText(entry.content, 'div');
+    }
+  }
+
+  return div;
+};
+
+var getInnerText = function (htmlString, selector) {
+  var dummyEl = document.createElement('div');
+  dummyEl.innerHTML = htmlString;
+  if (selector === 'div') {
+    return dummyEl.innerText;
+  } else {
+    var result = dummyEl.getElementsByTagName(selector);
+  }
+
+  var innerText;
+  if (result.length > 1) {
+    innerText = result[0].innerText.length >= result[1].innerText.length ? result[0].innerText : result[1].innerText;
+  } else if (result.length > 0) {
+    innerText = result[0].innerText;
+  }
+
+  return innerText;
 };
 
 module.exports = ParseHTMLMixin;
